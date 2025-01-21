6020e10d55dd1b0eaeec792180c6b06460c99e5c
Moiz Bootwala <124419659+RIZZZIOM@users.noreply.github.com>
2025-01-21 11:30:40 +0530

    added documentation basic template

---
+++ frontend/static/js/documentation.js
@@ -0,0 +1,7 @@
+// Smooth scroll to section
+function scrollToSection(sectionId) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
+}
