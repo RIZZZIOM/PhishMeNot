cfe95257f9cc8d92c1f387ccf4be210c8a6517a4
Moiz Bootwala <124419659+RIZZZIOM@users.noreply.github.com>
2025-01-21 09:42:52 +0530

    Added basic dashboard template

---
+++ frontend/static/js/index.js
@@ -0,0 +1,14 @@
+function createCampaign() {
+    alert("Redirecting to 'Create New Campaign' page...");
+    // Add redirection logic here
+}
+
+function viewResults() {
+    alert("Redirecting to 'View Results' page...");
+    // Add redirection logic here
+}
+
+function viewTemplates() {
+    alert("Redirecting to 'Templates' page...");
+    // Add redirection logic here
+}
