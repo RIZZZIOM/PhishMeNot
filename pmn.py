9f7485a58ebf83af4f817d89e035c14879adf48b
Moiz Bootwala <124419659+RIZZZIOM@users.noreply.github.com>
2025-02-24 17:28:56 +0530

    removed comments

--- pmn.py
+++ pmn.py
@@ -2,12 +2,10 @@ import subprocess
 import sys
 import os
 
-# List of required Python packages
 required_packages = [
     "flask", "pyyaml", "jinja2", "datetime", "smtplib"
 ]
 
-# Function to install missing dependencies
 def install_packages():
     for package in required_packages:
         try:
@@ -16,10 +14,8 @@ def install_packages():
             print(f"Installing {package}...")
             subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-# Install required packages
 install_packages()
 
-# Run the Flask app
 app_path = os.path.join("web", "app.py")
 if os.path.exists(app_path):
     print("Starting Phish Me Not application...")
